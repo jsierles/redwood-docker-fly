b7872e1ed9d3ece4c49dfff1b36ed784ad92d10f
Joshua Sierles <joshua@chatterbug.com>
2021-09-09 20:39:04 +0200

    Change a file to see how it affects incremental Docker builds

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -2,7 +2,7 @@ const AboutPage = () => {
   return (
     <p>
       This site was created to demonstrate my mastery of Redwood: Look on my
-      works, ye mighty, and despair!
+      works, ye mighty, and despair! Whee!
     </p>
   )
 }
